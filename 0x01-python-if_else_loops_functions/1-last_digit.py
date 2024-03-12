703e04ebb9db10d17032e09472ef5db6dcd59182
Makenalyn <makenalynton@gmail.com>
2024-03-12 12:19:42 -0700

    doesnt print negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
+if number < 0:
+    number = number * -1
 module = number % 10
+
 if module > 5:
     print(f"Last digit of {number} is {module} and is greater than 5")
 if module == 0:
     print(f"Last digit of {number} is {module} and is 0")
-if module > 0 & module < 6:
+if module > 0 and module < 6:
     print(f"Last digit of {number} is {module} and is less than 6 and not 0")
