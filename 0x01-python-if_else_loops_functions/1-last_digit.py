15e855b662ab5f857835566429535402ef932d80
Makenalyn <makenalynton@gmail.com>
2024-03-12 12:01:28 -0700

    checks several condition. not pycodestyle effectiv

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 module = number % 10
-if module is > 5:
+if module > 5:
     print(f"Last digit of {number} is {module} and is greater than five")
 if module == 0:
     print(f"Last digit of {number} is {module} and is zero")
