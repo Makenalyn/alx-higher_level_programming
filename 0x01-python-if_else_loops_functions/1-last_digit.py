9303e2f6a3424444e2a4072df17cd32765a302f2
Makenalyn <makenalynton@gmail.com>
2024-03-12 12:03:56 -0700

    rectified typos

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,6 @@ module = number % 10
 if module > 5:
     print(f"Last digit of {number} is {module} and is greater than 5")
 if module == 0:
-    print(f"Last digit of {number} is {module} and is zero")
+    print(f"Last digit of {number} is {module} and is 0")
 if module < 6:
-    print(f"Last digit of {number} is {module} and is less than 6 and not zero")
+    print(f"Last digit of {number} is {module} and is less than 6 and not 0")
