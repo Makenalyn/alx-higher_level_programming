aa74622237576be02d03b3a3916b6897f3c24897
Makenalyn <makenalynton@gmail.com>
2024-03-20 18:38:24 -0700

    compares two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    for x in set_1:
-        for y in set_2:
-            if x == y:
-                return x or y
+    return (set_1 & set_2)
