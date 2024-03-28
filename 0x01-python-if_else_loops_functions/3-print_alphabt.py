e3ba7973ab724bdc56fcb3e5b3e36439e06445b5
Makenalyn <makenalynton@gmail.com>
2024-03-28 05:40:40 -0700

    skips the specified alphabets

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for a in range(97, 123):
-    if a == 113:
+    if a == 101 or a == 113:
         continue
-    print(format(chr(a)), end="")
+    print('{}'.format(chr(a)), end="")
