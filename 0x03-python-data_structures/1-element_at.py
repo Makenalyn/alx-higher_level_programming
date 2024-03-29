3114c0a5e92b0c56a48bfce06b8c95b52b0ae9d9
Makenalyn <makenalynton@gmail.com>
2024-03-29 04:17:45 -0700

    retrieves an element from a list like c

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    for i in my_list:
+    for i in range(len(my_list)):
         if idx > i:
             return None
         elif idx < 0:
             return None
         else:
             return my_list[idx]
+
+        
