2a529d6c54bd07cdc653abc000294294acf9e2d0
Makenalyn <makenalynton@gmail.com>
2024-04-02 12:44:34 -0700

    attemps to create instances of the class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    def size(self):
+        return size
+
+    def size(self, value):
+        self.value = value
+
+    def area(self):
+        return size**size
