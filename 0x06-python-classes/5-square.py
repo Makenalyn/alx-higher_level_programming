fb57a3ec9e5805bf1878b9454137f35cbfa419aa
Makenalyn <makenalynton@gmail.com>
2024-04-02 12:48:26 -0700

    ...

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    def size(self):
+        return size
+
+    def size(self, value):
+        self.value = value
+
+    def area(self):
+        return self*self
+
+    def my_print(self):
+        print('{}'.format(size*size))
+
+
