1238c872ca54728e6b463c403662e15cb9195910
Makenalyn <makenalynton@gmail.com>
2024-04-03 01:43:34 -0700

    pycoded and added a private instance attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+""" class square that defined a class square"""
+
+
 class Square:
+    """ private instance size"""
+    __size = None
+
+    """ instantiation with size """
     def __init__(self, size):
         self.size = size
