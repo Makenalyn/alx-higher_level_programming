79e46241354bf269ac53a678776833bd090af642
Makenalyn <makenalynton@gmail.com>
2024-04-08 12:02:10 -0700

    rectangle class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" Class Rectangle defining rectangle """
+
+
+class REctangle:
+    def __init__(self, width=0, height=0):
+        self.__width = 0
+        self.__height = 0
+
+    def width(self):
+        return self.__width
+
+    def height(self):
+        return self.__height
+
+    def width(self, value):
+        return self.value
+
+    def height(self, value):
+        return self.value
+
+    if width < 0:
+        raise ValueError
+
+    if height < 0:
+        reuse ValueError
