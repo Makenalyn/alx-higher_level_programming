47165cce10600c93d9b61107b6c8f66aaf2641aa
Makenalyn <makenalynton@gmail.com>
2024-04-16 12:25:45 -0700

    returns an pbject represented by JSON string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ functions returns ab object represented by a json string """
-import json
+
 
 def from_json_string(my_str):
-    return json.loads(my_str) 
+
+    """ return tan object represented by a json string """
+    return json.loads(my_str)
