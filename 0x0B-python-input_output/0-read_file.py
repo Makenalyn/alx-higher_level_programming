3b905fd5b3f647c48d99ea63d6cab1095ff9da8d
Makenalyn <makenalynton@gmail.com>
2024-04-16 10:09:35 -0700

    pycoded and commented the function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+""" function reads a text from a file"""
+
+
 def read_file(filename=""):
+
+    """uses with to open file """
     with open(filename, encoding="utf-8") as f:
         write_data = f.read()
         print(write_data)
