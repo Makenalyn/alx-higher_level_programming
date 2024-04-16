3bc14a4b3f60a99e85704ebffc30737349659d2e
Makenalyn <makenalynton@gmail.com>
2024-04-16 12:47:49 -0700

    added documentation on module importation

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" import json module """
 import json
 
 """ function returns JSON representation of an onject """
@@ -9,5 +10,7 @@ def to_json_string(my_obj):
     """ doesn't manage exceptions
 
     parameter: object needing to be repsented
+
+    return: json rep of a file
     """
-    return json.dumps(my_obj)
+    return json.dumps([my_obj])
