b1caae11c4008bd924dde20e2aeb25ad3e6e4e9d
Makenalyn <makenalynton@gmail.com>
2024-04-16 12:50:24 -0700

    pycoded the file

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-""" import json module """
+""" import json module to represent the json module"""
 import json
-
-""" function returns JSON representation of an onject """
 
 
 def to_json_string(my_obj):
-
-    """ doesn't manage exceptions
-
-    parameter: object needing to be repsented
-
-    return: json rep of a file
-    """
+    """ return the JSON rep of a string object """
     return json.dumps([my_obj])
