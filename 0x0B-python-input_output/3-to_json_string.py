f207c5336596ddb38f81b821049faf2861c295f3
Makenalyn <makenalynton@gmail.com>
2024-04-16 12:56:14 -0700

    pycoded the function

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,7 +2,13 @@
 """ import json module to represent the json module"""
 import json
 
-
+""" function returns a string rep of a JSON notation """
 def to_json_string(my_obj):
-    """ return the JSON rep of a string object """
-    return json.dumps([my_obj])
+    """
+    return a string from the json object
+
+    my_obj: object to return JSON representation from
+
+    Return: string object
+    """
+    return json.dumps(my_obj)
