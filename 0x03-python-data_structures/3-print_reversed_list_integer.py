2f6bf313d8b0ead84c39f2003a90c98164589219
Makenalyn <makenalynton@gmail.com>
2024-04-25 09:42:56 -0700

    returns a new copy of string reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print('{}'.format(my_list[i]))
+    copy_list = my_list.copy()
+    copy_list.reverse()
+    for i in range(len(copy_list)):
+        print('{}'.format(copy_list[i]))
