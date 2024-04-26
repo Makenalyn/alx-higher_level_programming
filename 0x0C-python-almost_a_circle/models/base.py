108166903094a2c9c55f0bd07e84aae83dca0159
Makenalyn <makenalynton@gmail.com>
2024-04-26 15:29:02 -0700

    class assigns a new id to every new object instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,9 +7,8 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        self.id = None
-
-    if id is not None:
-        id = id
-    else:
-        _nb_objects += 1
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
