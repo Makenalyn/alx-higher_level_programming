cf81e9e1ffd23793d26c9cc2fa97a174ff484b12
Makenalyn <makenalynton@gmail.com>
2024-04-26 15:07:10 -0700

    pycoded the code

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" checks if an obj is an instance of class """
+
 
 def is_kind_of_class(obj, a_class):
+    """ if it is, return True, else, False """
     if isinstance(obj, a_class):
         return True
     else:
