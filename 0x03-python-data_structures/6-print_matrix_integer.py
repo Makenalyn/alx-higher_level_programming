3dec1aa89617611ef7c8ab0cdeee52834061e986
Makenalyn <makenalynton@gmail.com>
2024-05-01 11:34:13 -0700

    print integers of matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-if __name__=="__main__":
-    def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=[[]]):
         for i in matrix:
             print('{}'.format(i))
