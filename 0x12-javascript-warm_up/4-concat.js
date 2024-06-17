01be51e605cc31072fc1f2686d94fbb139f54365
Makenalyn <makenalynton@gmail.com>
2024-06-17 10:47:18 -0700

    using console.log to combine args

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-argv.forEach((val) => {
-	if (process.argv[2])
-	{
-		console.log(`${val} is ${val}`);
-	}
-});
+console.log(process.argv[2] + ' is ' + process.argv[3]);
