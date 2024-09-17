561ff779114aa986ca45cd03a22b79c5d640f62c
Makenalyn <makenalynton@gmail.com>
2024-09-17 11:06:55 -0700

    using exceptions with try

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    if not isinstance(a, int):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, int):
-        raise TypeError("b must be an int")
+    try:
+        isinstance(a, int)
+    except TypeError:
+        print("a must be an integer")
+    try:
+        isinstance(b, int)
+    except TypeError:
+        print("b must be an int")
