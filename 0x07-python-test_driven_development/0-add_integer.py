c49f0394b36106dae4ac34594e11377817494b1f
Makenalyn <makenalynton@gmail.com>
2024-09-17 11:02:02 -0700

    adds two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    if a or b not int:
+    if not isinstance(a, int):
         raise TypeError("a must be an integer")
-    if b not int:
-        raise TypeError("b must be an integer")
-
-    return a + b
+    if not isinstance(b, int):
+        raise TypeError("b must be an int")
