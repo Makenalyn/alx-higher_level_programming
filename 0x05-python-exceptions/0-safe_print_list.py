5db4222ac476e533b5ecb051de723eeda08bc941
makenalyn <makenalynton@gmai.com>
2025-02-24 16:04:37 -0800

    attempt to print a number of elements in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        print("{}".format(my_list[0:x]))
-    except ValueError as e:
+        for i in range(0, x):
+            print("{}".format(my_list[i]))
+    except Error as e:
         print(e)
