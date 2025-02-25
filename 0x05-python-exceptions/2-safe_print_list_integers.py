1be4e3082a41d61e0accf266c0a24004ae145bbb
makenalyn <makenalynton@gmai.com>
2025-02-24 16:15:22 -0800

    half done to safely print ints in a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    for i in my_list:
+    for i in range(0, x):
         try:
-            if i == int:
+            if isinstance(my_list[i], int):
                 print("{:d}".format())
         except ValueError:
             print("value is not an integer")
