76cc38502f0bc3e3c6dcca8d521ec3a512d0d8fe
makenalyn <makenalynton@gmai.com>
2025-02-24 16:34:22 -0800

    using try, except and finally

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    return a / b
+    try:
+        a / b
+    except ZeroDivisionError as e:
+        print("{}".format(e))
+    finally:
+        return a / b
